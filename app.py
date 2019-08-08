f3048f20cb1965ad94399d03bbb00c8aa9b0a14b
Luigi Langella <luigi76langella@gmail.com>
2019-08-08 18:48:15 +0100

    fixed bug in the edit categories button

--- app.py
+++ app.py
@@ -10,32 +10,38 @@ app.config["MONGO_URI"] = 'mongodb+srv://luigi76langella:Marilena7376@myfirstclu
 
 mongo = PyMongo(app)
 
-@app.route("/")
-@app.route("/get_tasks")
+@app.route('/')
+@app.route('/get_tasks')
 def get_tasks():
-    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
-@app.route("/add_task")
+
+@app.route('/add_task')
 def add_task():
-    return render_template('addtask.html', categories=mongo.db.categories.find())
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks = mongo.db.tasks
+    tasks =  mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
-    all_categories = mongo.db.categories.find()
-    return render_template("/edittask.html", task=the_task, categories=all_categories)
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
 
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
-    tasks.update( 
-    {'_id': ObjectId(task_id)},
+    tasks.update( {'_id': ObjectId(task_id)},
     {
         'task_name':request.form.get('task_name'),
         'category_name':request.form.get('category_name'),
@@ -45,30 +51,38 @@ def update_task(task_id):
     })
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/get_categories')
 def get_categories():
-   return render_template("categories.html", categories=mongo.db.categories.find())
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
 
-@app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id):
-    return render_template("editcategory.html", category=mongo.db.categories.find_one(
-                                                {'_id':ObjectId(category_id)}))
-
-@app.route('/edit_category/<category_id>')
-def edit_category(category_id):
-    mongo.db.categories.update({'_id':ObjectId(category_id)},
-                               {'category_name': request.form.get('category_name')})
-    return redirect(url_for('get_categories'))
 
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
-    mongo.db.categories.remove({'_id':ObjectId(category_id)})
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
 
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
@@ -82,10 +96,10 @@ def add_category():
     return render_template('addcategory.html')
 
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-    port=(os.environ.get("PORT")),
-    debug=True)
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=(os.environ.get('PORT')),
+            debug=True)
 
 
 
